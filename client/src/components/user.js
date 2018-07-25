552140a2659f1e1cc41d751100a16046bd56e9eb
ldanke <lukas.danke@outlook.de>
2018-07-26 00:12:21 +0200

    finished login function. everything is working.

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -4,30 +4,24 @@ class User extends Component{
 
     constructor(props) {
         super(props);
-        /*
-        * TODO (userObj+) googleId, name, email, token nur zum Entwickeln, später aus App holen und verarbeiten
-        * */
         this.state = {
             isRegistered: null,
             userObj: {},
-            tickets: []
+            tickets: [],
         }
     }
     componentDidMount(){
-        /*console.log("TEST CREATE");
-        this.newUser(JSON.parse(sessionStorage.getItem("userData")));*/
-        if(this.state.isRegistered===null){
-            console.log("check user");
+        if(this.state.isRegistered === null){
             this.checkUser();
+        }else if(this.state.isRegistered === false){
+            this.processUserValidation();
         }
     }
 
     processUserValidation = () => {
         if(this.state.isRegistered){
-            console.log("userObj exists ");
             this.getTickets();
         }else{
-            console.log("creating new user");
             this.newUser(JSON.parse(sessionStorage.getItem("userData")));
         }
     };
@@ -35,16 +29,19 @@ class User extends Component{
     checkUser = () => {
         fetch('http://localhost:3000/users/'+JSON.parse(sessionStorage.getItem("userData")).googleid)
             .then(this.fetchStatusHandler)
-            .then(response => this.setState({isRegistered:true}))
+            .then(x => this.setState({isRegistered:true}))
             .then(this.processUserValidation)
-            .catch(function (error){
-                console.log('Parsing user failed ',error);
-            });
+            .catch(error => this.gotError(error));
 
     };
 
+    gotError = (error) => {
+        console.log("user doesn't exist", error);
+        this.setState({isRegistered:false});
+        this.processUserValidation()
+    };
+
     fetchStatusHandler(response) {
-        console.log("check statuscode", response.status);
         if (response.status >= 200 && response.status < 300) {
             return response;
         } else {
@@ -53,9 +50,7 @@ class User extends Component{
     }
 
     getTickets = () => {
-        console.log("get tickets");
         fetch('http://localhost:3000/users/'+JSON.parse(sessionStorage.getItem("userData")).googleid)
-            .then(this.fetchStatusHandler)
             .then(response => response.json())
             .then(parsedJSON => this.setState({tickets: parsedJSON.user.tickets}))
             .then(x => localStorage.setItem('tickets', JSON.stringify(this.state.tickets)))
@@ -67,12 +62,11 @@ class User extends Component{
         fetch('/users/',
             {
                 method: 'POST',
-                body:{
-                    googleid: userData.googleid,
-                    name: userData.name,
-                    tickets: [],
-                    cuid: userData.token
-                }
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({user:{googleid: userData.googleid,
+                        name: userData.name,
+                        tickets: [],
+                        cuid: userData.token}})
             }
         )
             .then(this.fetchStatusHandler)
